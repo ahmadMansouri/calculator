6e5a937789d7ec534906d85176384e50be146b74
Student <Student@A11-PC>
2017-07-13 16:56:53 +0430

    Add project files.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public double Divide(double firstNum, double secondNum)
+        {
+            var result = 0;
+
+            return result;
+        }
+    }
+}
