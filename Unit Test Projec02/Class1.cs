be4db62a68c8dad3b233e5d702e7632831d9c61f
AidinAlhverdi <Avd1357@gmail.com>
2017-07-15 15:34:59 +0430

    1

---
+++ Unit Test Projec02/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_Test_Projec02
+{
+    public class Class1
+    {
+    }
+}
